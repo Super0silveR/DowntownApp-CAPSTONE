397690af832de253421919ee6e225adede85fdd1
Vincent Harvey <harveyvincent1@gmail.com>
2023-01-22 21:28:49 -0500

    initial work on the skeleton for the api and docker.

---
+++ Application/Core/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Core
+{
+    public class Result<T>
+    {
+        public string? Error { get; set; }
+        public bool IsSuccess { get; set; }
+        public T? Value { get; set; }
+
+        public static Result<T> Success(T value) => new() { IsSuccess = true, Value = value };
+
+        public static Result<T> Failure(string error) => new() { IsSuccess = false, Error = error };
+    }
+}
