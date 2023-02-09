18ca2f11fedb759c3d1b72d4f67fc97dbce7c0bd
Vincent Harvey <harveyvincent1@gmail.com>
2023-02-08 22:16:03 -0500

    added some claims oriented verification with UAuth0.

---
+++ Api/Requirements/Handlers/HasScopeHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Api.Requirements.Handlers
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                       HasScopeRequirement requirement)
+        {
+            if (!context.User.HasClaim(claim => claim.Type == "scope" && claim.Issuer == requirement.Issuer))
+                return Task.CompletedTask;
+
+            var scopes = context.User
+                                .FindFirst(claim => claim.Type == "scope" && claim.Issuer == requirement.Issuer)!
+                                .Value
+                                .Split(" ");
+
+            if (scopes.Any(scope => scope == requirement.Scope))
+                context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
