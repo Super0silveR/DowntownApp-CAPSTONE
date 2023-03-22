9347d96438f5a3c179eacf391ad74795c9c7f72c
nwahb063 <nwahb063@uottawa.ca>
2023-03-22 03:58:31 -0400

    integration zoom

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,9 +6,32 @@ import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
 
+import ZoomComponent from './components/ZoomMeeting';
+
+const App: React.FC = () => {
+  // Define the Zoom meeting details
+  const apiKey = 'P0Dx0N05RKOUHsHeXDoD6g';
+  const signature = 'YOUR_ZOOM_SIGNATURE';
+  const meetingNumber = 'YOUR_ZOOM_MEETING_NUMBER';
+
+  return (
+    <div className="App">
+      <ZoomComponent
+        apiKey={apiKey}
+        signature={signature}
+        meetingNumber={meetingNumber}
+      />
+    </div>
+  );
+};
+
+export default App;
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
     /** Providing our store context to the application. */
     <StoreContext.Provider value={store}>
