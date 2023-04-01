0ba37e2f1e73a78f9c9f00370a7901fcadce450f
nwahb063 <nwahb063@uottawa.ca>
2023-03-31 23:05:26 -0400

    Home Page

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,84 +1,174 @@
-import { Container, Link as MuiLink, Typography } from '@mui/material';
+import { Container, Link as MuiLink, Typography, AppBar, Button, IconButton, Toolbar, Box} from '@mui/material';
 import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
 import { useStore } from '../../app/stores/store';
 import LoginForm from '../users/LoginForm';
 import RegisterForm from '../users/RegisterForm';
+import monImage from '../../features/home/image.png';
 
 function HomePage() {
+  const { modalStore, userStore } = useStore();
 
-    const { modalStore, userStore } = useStore();
+  const handleLoginClick = () => {
+    // Handle login logic here
+  };
 
-    return (
-        <>
-            <Container sx={{mt:'7em'}}>
-                <Typography 
-                    variant='h3' 
-                    fontWeight={600} 
-                    fontFamily='monospace'
-                    sx={{
-                        textDecoration:'underline'
-                    }}
+  const handlePasswordClick = () => {
+    // Handle password logic here
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            variant="h3"
+            fontWeight={600}
+            fontFamily="-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', 'Fira Sans', Ubuntu, Oxygen, 'Oxygen Sans', Cantarell, 'Droid Sans', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Lucida Grande', Helvetica, Arial, sans-serif;"
+
+            sx={{
+              fontSize: '2.5rem'
+            }}
+          >
+            Downtown
+          </Typography>
+
+          <div>
+            {userStore.isLoggedIn ? (
+              <Button
+                component={Link}
+                to="/events"
+                variant="contained"
+                color="secondary"
+                sx={{ mr: 2 }}
+              >
+                Events
+              </Button>
+            ) : (
+              <>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  onClick={() => modalStore.openModal(<LoginForm />)}
+                  sx={{ mr: 1,  borderRadius: '50px' }}
                 >
-                    Downtown
-                </Typography>
-                <Typography 
-                    variant='body1' 
-                    fontFamily='monospace'
-                    sx={{
-                        mb:2,
-                        fontSize: 18
-                    }}
+                  Login
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => modalStore.openModal(<RegisterForm />)}
+                  sx={{ borderRadius: '50px' }}
                 >
-                    Let loose & connect!
-                </Typography>
-                { userStore.isLoggedIn 
-                    ? (
-                        <Typography 
-                            variant='h5' 
-                            fontWeight={500} 
-                            fontFamily='monospace'
-                        >
-                            Go to our listed <Link to='/events'>Events</Link>!
-                        </Typography>
-                    ) 
-                    : (
-                        <>
-                            <Typography 
-                                variant='h5' 
-                                fontWeight={500} 
-                                fontFamily='monospace'
-                            >
-                                <MuiLink 
-                                    component='button' 
-                                    onClick={() => modalStore.openModal(<LoginForm />)}
-                                    variant='overline'
-                                    fontWeight={600}
-                                    sx={{
-                                        textDecoration:'none',
-                                        fontSize: 20
-                                    }}
-                                >
-                                    Login
-                                </MuiLink> to start your adventure!
-                            </Typography>
-                            <MuiLink 
-                                component='button' 
-                                onClick={() => modalStore.openModal(<RegisterForm />)}
-                                variant='overline'
-                                fontWeight={600}
-                                sx={{
-                                    textDecoration:'none',
-                                    fontSize: 20
-                                }}
-                            >
-                                Register
-                            </MuiLink>
-                        </>
-                    )}
-            </Container>
-        </>
-    );
-}; 
+                  Register
+                </Button>
+              </>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+      
 
+      <Container sx={{ mt: '7em',
+       // Remplacez cette couleur par celle que vous souhaitez utiliser
+     }}>
+        <Typography
+          variant="h3"
+          font-size =" 32px"
+          font-weight= "300"
+          fontFamily="-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', 'Fira Sans', Ubuntu, Oxygen, 'Oxygen Sans', Cantarell, 'Droid Sans', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Lucida Grande', Helvetica, Arial, sans-serif;"
+          sx={{
+            color: '#8f5849',
+            mb: '1em'
+          }}
+        >
+          Welcome to <br/> Downtown !
+        </Typography>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <p>Welcome to our online platform! With our convenient downtown application< br/> you can easily attend and discover events anytime, anywhere < br/> all from the comfort of your own device.</p>
+        </div>
+      </div>
+    </div>
+          
+          <>
+
+          
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', marginTop: '3em' }}>
+  <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '0.5em' }}>
+    <div style={{ marginBottom: '0.5em' }}>E-mail</div>
+    <input
+      type="email"
+      style={{
+        padding: '1em',
+        fontSize: 16,
+        borderRadius: 4,
+        border: '1px solid #ccc',
+        width: '360px'
+      }}
+    />
+  </div>
+  <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '1em' }}>
+  <div style={{ marginBottom: '0.5em' }}>Password</div>
+    <input
+      type="password"
+      style={{
+        padding: '1em',
+        fontSize: 16,
+        borderRadius: 4,
+        border: '1px solid #ccc',
+        width: '360px'
+      }}
+    />
+  </div>
+  <div style={{ display: 'flex', justifyContent: 'space-between' }}></div>
+  <Button variant="contained" 
+   onClick={() => modalStore.openModal(<LoginForm/>)} style={{ width: '360px',  borderRadius: '25px',  height: '50px'}}>
+  Let loose & connect!
+  </Button>
+</div>
+
+   </>
+      </Container>
+      <Box
+        component="img"
+        src={monImage}
+        alt="ma photo"
+        sx={{
+            position: 'fixed',
+            top: '50%',
+            right: 0,
+            transform: 'translateY(-50%)',
+
+            width: '500px',
+            height: '500px',
+            objectFit: 'cover',
+            zIndex: 1,
+            
+            borderRadius: '50px'
+        }}
+      />
+    </>
+  );
+    
+    }
+
+
+    function Accueil() {
+        return (
+          <Box
+            sx={{
+              backgroundColor: '#f0f0f0', // Remplacez cette couleur par celle que vous souhaitez utiliser
+              width: '100vw',
+              height: '100vh',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+         </Box>
+        );
+    }
 export default observer(HomePage);
