d51896c5e9c88083fab17ada4197bdda8fc24a3a
BearWulF <elias.sidi@gmail.com>
2023-04-03 15:00:09 -0400

    x

--- client-app/src/features/Mailbox/MailboxMessagesContents.tsx
+++ client-app/src/features/Mailbox/MailboxMessagesContents.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function MailboxMessagesContents() {
   return (
-    <p>This is the message content beep boop</p>
+      <p>This is the message content beep boop</p>
   );
 }
 
