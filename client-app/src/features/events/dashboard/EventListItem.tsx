408ed635b53edc75e9aedf3f94b322e0147d358f
Younes Anys <yanys048@uottawa.ca>
2023-09-16 11:54:01 -0400

    changed color of event boxes

--- client-app/src/features/events/dashboard/EventListItem.tsx
+++ client-app/src/features/events/dashboard/EventListItem.tsx
@@ -33,7 +33,7 @@ function EventListItem({ event }: Props) {
         >
             <Card
                 sx={{
-                    backgroundColor: '#ffb9dc', 
+                    backgroundColor: '#be29ec', 
                     borderRadius: '0.3em',
                     boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
                 }}
