87540afdc383722343afbb3317cad49adc020046
Vincent Harvey <harveyvincent1@gmail.com>
2023-10-14 13:18:46 -0400

    Backend changes related to creator profiles.

---
+++ Application/Validators/ProfileCommandDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs.Commands;
+using FluentValidation;
+
+namespace Application.Validators
+{
+    /// <summary>
+    /// Abstract validator for the command DTO for updating user profile general information.
+    /// </summary>
+    public class ProfileCommandDtoValidator : AbstractValidator<ProfileCommandDto>
+    {
+        public ProfileCommandDtoValidator()
+        {
+            RuleFor(pcdto => pcdto.DisplayName)
+                .NotEmpty()
+                .MinimumLength(6);
+
+            RuleFor(pcdto => pcdto.Bio)
+                .MinimumLength(50)
+                .MaximumLength(255);
+        }
+    }
+}
