36fb582f731f89f2343ca20e6505d4f1a87cd90d
Younes Anys <yanys048@uottawa.ca>
2023-11-04 14:39:16 -0400

    setting background image for event boxes

--- client-app/src/features/events/dashboard/EventListItem.tsx
+++ client-app/src/features/events/dashboard/EventListItem.tsx
@@ -26,6 +26,7 @@ import dayjs from 'dayjs';
 import { useTheme } from '@mui/material/styles';
 import { Favorite, GroupAdd, Share } from '@mui/icons-material';
 import EventOptionsMenu from '../../../app/layout/menus/EventOptionsMenu';
+import Image3 from '../../../assets/image.png';
 
 interface Props {
     event: Event;
@@ -52,11 +53,19 @@ function EventListItem({ event }: Props) {
 
     return (
         <Grid item xs={12} key={event.id} sx={{  }}>
-            <Card sx={{  }}>
+            <Card
+                sx={{
+                    backgroundImage: `url(${Image3})`,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                }}
+
+            >
                 <CardHeader
                     avatar={
                         <Tooltip title={host?.displayName} arrow>
-                            <Avatar src={host?.photo || '/assets/user.png'} sx={{  }} />
+                            <Avatar src={host?.photo || '../../../assets/girl.png'} sx={{  }} />
                         </Tooltip>
                     }
                     action={<EventOptionsMenu event={event} />}
