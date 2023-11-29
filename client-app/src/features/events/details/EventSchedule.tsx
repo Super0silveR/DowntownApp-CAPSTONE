e5016b0f2332f746fae642cb1682e3803f7ed7b5
Younes Anys <yanys048@uottawa.ca>
2023-11-28 22:38:49 -0500

    added remote/in-person switch in the frontend in the event scheduling

--- client-app/src/features/events/details/EventSchedule.tsx
+++ client-app/src/features/events/details/EventSchedule.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { TextField, IconButton, Grid, Typography, Paper, Button } from '@mui/material';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { TextField, IconButton, Grid, Typography, Paper, Button, Switch, FormControlLabel } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { useStore } from '../../../app/stores/store';
 
 export interface EventSchedule {
     id: number;
     date: string;
     location: string;
     barId: string;
+    isRemote: boolean;
+    address?: string; // Optional address field for in-person events
 }
 
 interface Props {
@@ -16,16 +16,14 @@ interface Props {
     setSchedules: React.Dispatch<React.SetStateAction<EventSchedule[]>>;
 }
 
-export const EventSchedule: React.FC<Props> = ({ schedules, setSchedules }) => {
-    const [newSchedule, setNewSchedule] = useState<EventSchedule>({ id: Date.now(), date: '', location: '', barId: '' });
-    const { eventStore } = useStore(); 
-
-    const handleAddSchedule = () => {
-        if (newSchedule.date && newSchedule.location) {
-            setSchedules([...schedules, newSchedule]);
-            setNewSchedule({ id: Date.now(), date: '', location: '', barId: '' }); 
-        }
-    };
+const EventScheduleComponent: React.FC<Props> = ({ schedules, setSchedules }) => {
+    const [newSchedule, setNewSchedule] = useState<EventSchedule>({
+        id: Date.now(),
+        date: '',
+        location: '',
+        barId: '',
+        isRemote: true
+    });
 
     const handleDateChange = (id: number, date: string) => {
         setSchedules(schedules.map(schedule => schedule.id === id ? { ...schedule, date } : schedule));
@@ -35,14 +33,19 @@ export const EventSchedule: React.FC<Props> = ({ schedules, setSchedules }) => {
         setSchedules(schedules.map(schedule => schedule.id === id ? { ...schedule, location } : schedule));
     };
 
+    const handleAddressChange = (id: number, address: string) => {
+        setSchedules(schedules.map(schedule => schedule.id === id ? { ...schedule, address } : schedule));
+    };
+
+    const handleToggleRemote = (id: number) => {
+        setSchedules(schedules.map(schedule => schedule.id === id ? { ...schedule, isRemote: !schedule.isRemote } : schedule));
+    };
+
     const handleRemoveSchedule = (id: number) => {
         setSchedules(schedules.filter(schedule => schedule.id !== id));
     };
+
     const handleSaveSchedules = async () => {
-        if (newSchedule.date && newSchedule.location) {
-            await eventStore.scheduleEvent(newSchedule); 
-            setNewSchedule({ id: Date.now(), date: '', location: '', barId: '' }); 
-        }
     };
 
     return (
@@ -70,6 +73,28 @@ export const EventSchedule: React.FC<Props> = ({ schedules, setSchedules }) => {
                             />
                         </Grid>
                         <Grid item xs={2}>
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        checked={schedule.isRemote}
+                                        onChange={() => handleToggleRemote(schedule.id)}
+                                    />
+                                }
+                                label={schedule.isRemote ? 'Remote' : 'In-Person'}
+                            />
+                        </Grid>
+                        {!schedule.isRemote && (
+                            <Grid item xs={10}>
+                                <TextField
+                                    label="Address"
+                                    type="text"
+                                    value={schedule.address || ''}
+                                    onChange={(e) => handleAddressChange(schedule.id, e.target.value)}
+                                    fullWidth
+                                />
+                            </Grid>
+                        )}
+                        <Grid item xs={12}>
                             <IconButton onClick={() => handleRemoveSchedule(schedule.id)} color="error">
                                 <DeleteIcon />
                             </IconButton>
@@ -85,35 +110,52 @@ export const EventSchedule: React.FC<Props> = ({ schedules, setSchedules }) => {
                         fullWidth
                         InputLabelProps={{ shrink: true }}
                     />
-
                 </Grid>
                 <Grid item xs={5}>
                     <TextField
                         label="New Location"
                         type="text"
-                        value={newSchedule.location}
+                        value={newSchedule.location || ''}
                         onChange={(e) => setNewSchedule({ ...newSchedule, location: e.target.value })}
                         fullWidth
                     />
                 </Grid>
                 <Grid item xs={2}>
-                    <IconButton onClick={handleAddSchedule} color="primary">
-                        <AddCircleOutlineIcon />
-                    </IconButton>
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={newSchedule.isRemote}
+                                onChange={() => setNewSchedule({ ...newSchedule, isRemote: !newSchedule.isRemote })}
+                            />
+                        }
+                        label={newSchedule.isRemote ? 'Remote' : 'In-Person'}
+                    />
+                </Grid>
+                {!newSchedule.isRemote && (
+                    <Grid item xs={10}>
+                        <TextField
+                            label="New Address"
+                            type="text"
+                            value={newSchedule.address || ''}
+                            onChange={(e) => setNewSchedule({ ...newSchedule, address: e.target.value })}
+                            fullWidth
+                        />
+                    </Grid>
+                )}
+                <Grid item xs={12}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleSaveSchedules}
+                        fullWidth
+                        style={{ marginTop: '1em' }}
+                    >
+                        Save Schedules
+                    </Button>
                 </Grid>
             </Grid>
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={handleSaveSchedules}
-                style={{ marginTop: '1em' }}
-            >
-                Save Schedules
-            </Button>
-
         </Paper>
     );
 };
-export default EventSchedule;
 
-
+export default EventScheduleComponent;
